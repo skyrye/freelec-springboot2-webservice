7aecf8c6ae4122d161336ef0812d25bcebe4ce12
skyrye <se78kim@gmail.com>
2021-02-21 16:31:39 +0900

    2월 21일 테스트버전

--- IdeaProjects/freelec-springboot2-webservice/src/test/java/web/HelloControllerTest.java
+++ IdeaProjects/freelec-springboot2-webservice/src/test/java/web/HelloControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -19,8 +20,9 @@ public class HelloControllerTest {
     private MockMvc mvc;
 
     @Test
-    public void hello_리턴된다()   throws Exception{
+    public void hello_return()  throws Exception{
         String hello = "hello";
+
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
